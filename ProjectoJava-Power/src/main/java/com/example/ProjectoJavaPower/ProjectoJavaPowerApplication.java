1274db3893c7fad42d08ac423779e316a2395292
Sebastian Duque <jsebas000@gmail.com>
2022-08-25 01:01:15 +0200

    projecto para clonar por ramas y crear las clases

---
+++ ProjectoJava-Power/src/main/java/com/example/ProjectoJavaPower/ProjectoJavaPowerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectoJavaPower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoJavaPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoJavaPowerApplication.class, args);
+	}
+
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# grupojavapower
-Repositorio para el desarrollo del proyecto Desarrollo de software Ciclo 3 MIntic
