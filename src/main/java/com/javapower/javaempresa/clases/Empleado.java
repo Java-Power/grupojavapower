76e4304d56b5c9a8265a9710fab299a886bc3661
Rosy Rodriguez <109880264+RosyRodriguezz@users.noreply.github.com>
2022-08-25 21:00:04 -0500

    test

--- src/main/java/com/javapower/javaempresa/clases/Empleado.java
+++ src/main/java/com/javapower/javaempresa/clases/Empleado.java
@@ -18,4 +18,37 @@ public class Empleado {
 
         }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getEmpresaEmpleado() {
+        return empresaEmpleado;
+    }
+
+    public void setEmpresaEmpleado(String empresaEmpleado) {
+        this.empresaEmpleado = empresaEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+
+
 }
