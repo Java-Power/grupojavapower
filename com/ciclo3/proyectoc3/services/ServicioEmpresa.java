f4c2f79af3c8dfe77b7be2ae872ad0a1ea7e1479
johnrestrepo <educana.online@gmail.com>
2022-09-02 12:44:05 -0500

    Modificacion de paquetes

---
+++ com/ciclo3/proyectoc3/services/ServicioEmpresa.java
@@ -0,0 +1,31 @@
+package com.ciclo3.proyectoc3.services;
+
+public class ServicioEmpresa {
+
+    public ServicioEmpresa() {
+        super();
+    }
+
+    public void registrarEmpresa(String nombre, String direccion, String telefono, String correo, String contrasena) {
+
+    }
+
+    public void modificarEmpresa(String nombre, String direccion, String telefono, String correo, String contrasena) {
+
+    }
+
+    public void eliminarEmpresa(String nombre, String direccion, String telefono, String correo, String contrasena) {
+
+    }
+
+    public void buscarEmpresa(String nombre, String direccion, String telefono, String correo, String contrasena) {
+
+    }
+
+    public void listarEmpresa() {
+
+        // logica de negocios
+    }
+
+
+}
