dbea5e0984d4a3d61c0bbbaf5da09def4e14c0ff
Sebastian Duque <jsebas000@gmail.com>
2022-09-04 21:39:22 +0200

    configurados los servicios

--- com/ciclo3/proyectoc3/entities/Empresa.java
+++ com/ciclo3/proyectoc3/entities/Empresa.java
@@ -1,14 +1,39 @@
 package com.ciclo3.proyectoc3.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "empresa")
 public class Empresa {
+    //ATRIBUTOS
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "empresa_id")
+    private long id;
+    @Column(name = "nombre")
     private String nombre;
+    @Column(name = "direccion")
     private String direccion;
+    @Column(name = "telefono")
     private String telefono;
+    @Column(name = "NIT")
     private String NIT;
+
+
+// Relacion de unna empresa puede tener mucho empelados
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
+    private List<Empleado> empleados;
+
+ // Relacion de una empresa puede tener muchas transacciones o movimientos de dinero
+    @OneToMany(mappedBy = "empresa", cascade = CascadeType.ALL)
+    private List<MovimientoDinero> movimientoDineros;
+
 
     //CONTRUCTOR
 
-    public Empresa(String nombre, String direccion, String telefono, String NIT) {
+   public Empresa(String nombre, String direccion, String telefono, String NIT) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.telefono = telefono;
